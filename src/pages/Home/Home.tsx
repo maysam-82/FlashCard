22452cdfd66570b0eccba37dafc849ea14c86795
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-07-09 23:45:26 +1000

    created Home

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import StackTitleList from '../../components/StackTitleList/StackTitleList';
+
+const Home = () => {
+	return <StackTitleList />;
+};
+
+export default Home;
