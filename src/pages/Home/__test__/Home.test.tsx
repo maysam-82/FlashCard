d35982e48365d085b7d3798e3777f3b2811463e0
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-07-12 19:07:22 +1000

    Wrote test for Home

---
+++ src/pages/Home/__test__/Home.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Home from '../Home';
+
+describe('Home component', () => {
+	const component = shallow(<Home />);
+	it('should render Home component correctly', () => {
+		expect(component).toMatchSnapshot();
+	});
+});
